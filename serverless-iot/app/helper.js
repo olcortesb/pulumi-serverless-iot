1340accea53aa2311118a65cee59a55d41ef3911
Oscar Cortes <olcortesb@gmail.com>
2021-05-16 23:13:45 -0300

    feat: refactor helper.js

--- serverless-iot/app/helper.js
+++ serverless-iot/app/helper.js
@@ -4,22 +4,19 @@ const url = 'http://www.omdbapi.com/?apikey=9a044c5c&t=';
 const urllambda = 'https://kgczmkfv0c.execute-api.us-west-2.amazonaws.com/stage/data';
 
 async function processdata(event){
-    const { path, queryStringParameters, headers, body } = event;
-    const payloadBuffer = new Buffer.from(event.body, 'base64')
-
-    const payload = payloadBuffer.toJSON()
+    const payloadBuffer = new Buffer.from(event.body, 'base64')    
     const payloadString = payloadBuffer.toString('ascii')
 
-
-    console.log("payload to JSON " + payload);
-    console.log("Payload To String "+payloadString);
-
+    console.log("Payload To String " + payloadString);
 
     const obj = JSON.parse(payloadString);
 
-    const response = await axios.get(url + obj.message)
+    const response = await axios.get(url + obj.message);
 
-    const responseLambda = await axios.post(urllambda, { url: response.data.Poster})
+    const resp_lambda = await axios.post(
+        urllambda, 
+        { url: response.data.Poster}
+        );
 
     return {
         statusCode: 200,
